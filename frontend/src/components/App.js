ff27ed61d876e4c8084a811b0a9486884373af93
caitlynl22 <caitlyn.landry@gmail.com>
2018-08-08 09:47:12 -0400

    Add Header component
    Simple UI component to eventually link to favorites

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
+import Header from './Header';
 import MovieSearch from './MovieSearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <div className="main-container">
           <MovieSearch />
         </div>
